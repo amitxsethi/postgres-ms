4e4bd43536689a2f9d142eeebc4af4aa67899c72
amitsethi <amit.x.sethi@gmail.com>
2024-09-22 16:54:28 +0100

    my first commit

---
+++ src/main/java/com/example/postgres_ms/PostgresMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgres_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postgres_ms/PostgresMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgres_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
